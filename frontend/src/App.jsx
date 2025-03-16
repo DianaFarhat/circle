16aef157aa690afdf782d28498d35879c4b58fb9
DianaFarhat <dsf04@mail.aub.edu>
2025-03-16 22:12:46 +0200

    Add bootstrap to index.html & App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
 import { Outlet } from "react-router-dom";
-import Navigation from "./components/Navigation";
+import Navigation from "./pages/Auth/Navigation.jsx"; // Bootstrap Navbar
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store"; 
+import "bootstrap/dist/css/bootstrap.min.css"; // ✅ Bootstrap CSS globally
+import "bootstrap/dist/js/bootstrap.bundle.min"; // ✅ Bootstrap JS (for modals, dropdowns, etc.)
+
 import { Route, RouterProvider, createRoutesFromElements, createBrowserRouter } from "react-router-dom"; 
 import ReactDOM from "react-dom/client";
-
-
 
 function App() {
   return (
