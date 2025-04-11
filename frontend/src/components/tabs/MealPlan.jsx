174e480444951d212a7029c569a75dc214596bef
DianaFarhat <dsf04@mail.aub.edu>
2025-04-11 17:43:15 +0300

    Fix useEffect bug

--- frontend/src/components/tabs/MealPlan.jsx
+++ frontend/src/components/tabs/MealPlan.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
